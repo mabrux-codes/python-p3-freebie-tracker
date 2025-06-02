30ceac77eaa93d2f675835b2e465bcfb5d933d89
Abel Lukwaro <abelmabruke@gmail.com>
2025-06-02 10:09:46 +0300

    Initial Commit

--- lib/seed.py
+++ lib/seed.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python3
 
 # Script goes here!
-from models import Company,Dev,Freebie
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Company, Dev, Freebie
 
-#company1 = Company(name="ARC", founding_year=2018)
-#company2 = Company(name="MANY", founding_year=2020)
+engine = create_engine("sqlite:///freebies.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+Base.metadata.create_all(engine)
 
+company1 = Company(name="RAM", founding_year=2010)
+company2 = Company(name="ZORD", founding_year=2013)
+company3 = Company(name="AWAG", founding_year=2020)
 
-#dev1 = Dev(name="Daud")
-#dev2 = Dev(name="Allan")
+dev1 = Dev(name="Daud")
+dev2 = Dev(name="Gich")
+dev3 = Dev(name="Zanaa")
 
-#session.add_all([company1, company2, dev1, dev2])
-#session.commit()
+session.add_all([company1, company2, company3, dev1, dev2, dev3])
+session.commit()
 
-f1 = Freebie(item_name="Asus laptop", value=1, dev=dev1, company=company1)
-f2 = Freebie(item_name="PS 5", value=10, dev=dev2, company=company2)
+f1 = Freebie(item_name="OLED GAMING SCREEN", value=50000, dev=dev1, company=company1)
+f2 = Freebie(item_name="ROG PHONE", value=70000, dev=dev2, company=company2)
+f3 = Freebie(item_name="RAZER MOUSE", value=5000, dev=dev1, company=company2)
+f4 = Freebie(item_name="JBL", value=15000, dev=dev3, company=company3)
+f5 = Freebie(item_name="BUDS", value=10000, dev=dev2, company=company1)
 
-session.add_all([f1, f2])
+session.add_all([f1, f2, f3, f4, f5])
 session.commit()
